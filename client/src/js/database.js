114415f802be651a19c191c2d28bf1b387f253cc
gwenparadis <gwendelynparadis2@gmail.com>
2023-02-21 16:36:42 -0500

    complete database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Requirement: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("Update the database");
 
@@ -33,7 +33,26 @@ export const putDb = async (content) => {
   console.log("text edits saved to the database!", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+// Requirement: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("GET from the Database");
+
+  // Create connection to the database and version
+  const textEdit = await openDB("jate", 1);
+
+  // Create a new transaction and specify database and data privileges.
+  const tx = textEdit.transaction("jate", "readonly");
+
+  // Open up the desired object store
+  const store = tx.objectStore("jate");
+
+  // Pass in the content
+  const request = store.getAll();
+
+  // Confirm request
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
